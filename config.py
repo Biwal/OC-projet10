82f08e5c78fc85996778511f7a2786113c602b35
Guilhem MASSONNAT <guilhem.massonnat@yahoo.com>
2022-06-10 14:26:52 +0200

    update credentials

--- config.py
+++ config.py
@@ -8,5 +8,5 @@ class DefaultConfig:
     """ Bot Configuration """
 
     PORT = 3978
-    APP_ID = os.environ.get("MicrosoftAppId", "")
-    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "")
+    APP_ID = os.environ.get("MicrosoftAppId", "ac87b7f2-adaa-4e87-897b-a88608b2ef11")
+    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "bCM8Q~ySn5ktHPeGk41qAcLZniw2sOwRH545Wate")
